7a1a0b956971a4d85a31254aad2a63b6a737c3e7
Christian Lindholm <christian.lindholm@iki.fi>
2022-05-16 01:57:09 +0300

    basic listening for new device info

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const { logger, settings } = require("./globals");
 
 const mqtt = require("./mqtt");
 
+var devices = {};
+
+function assertDeviceData(id) {
+  if (!devices[id]) {
+    devices[id] = {};
+  }
+}
+
 function mqtt_listener(topic, message) {
-  logger.debug(`${topic}: ${message}`);
+  logger.debug(`${topic}: ${JSON.stringify(message)}`);
+  var mac = message.mac;
+  assertDeviceData(mac);
+  if (topic.endsWith("info")) {
+    devices[mac].info = message;
+  } else if (topic.endsWith("announce")) {
+    devices[mac].announce = message;
+  }
+  if()
 }
 
 mqtt.setListener(mqtt_listener);
